b3b09da30335492822e5b79c0fd76e9b26d67f19
Cherry NG <cherry92@gmail.com>
2014-04-28 18:51:27 +0800

    hide bounding box

--- assets/js/n-mario.js
+++ assets/js/n-mario.js
@@ -54,7 +54,7 @@ function create() {
   player.scale.setTo(3, 3);
   player.body.setSize(14, 16, 0, 24);
 
-  player.animations.add('walk', [3, 5, 6, 7, 2], 5, true);
+  player.animations.add('walk', [3, 6, 7, 2], 5, true);
   player.animations.play('walk');
   player.anchor.setTo(0.5, 0.5);
 
@@ -76,6 +76,7 @@ function update() {
     player.body.velocity.x = 300;
     player.animations.play('walk');
     player.scale.x = 3;
+    player.body.setSize(14, 16, 0, 24);
 
   } else {
     // stand still
@@ -90,5 +91,5 @@ function update() {
 }
 
 function render() {
-  game.debug.body(player);
+  // game.debug.body(player);
 }
