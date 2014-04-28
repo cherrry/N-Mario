459036659b30e3da3f8a1a0ee029f84e7a270ab1
Cherry NG <cherry92@gmail.com>
2014-04-28 17:37:21 +0800

    animated brick and question

--- assets/js/n-mario.js
+++ assets/js/n-mario.js
@@ -1,5 +1,5 @@
 var platforms;
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(48 * 15, 48 * 10, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
   game.load.image('sky', 'assets/sprites/3x/sky.png');
@@ -9,7 +9,35 @@ function preload() {
 
 function create() {
   // sky
-  game.add.tileSprite(0, 0, 800, 600, 'sky');
+  game.add.tileSprite(0, 0, game.world.width, game.world.height, 'sky');
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  // platform
+  platforms = game.add.group();
+  platforms.enableBody = true;
+
+  for (var i = 0; i < 15; i++) {
+    var ground = platforms.create(i * 48, game.world.height - 48, 'brick');
+    ground.body.immovable = true;
+  }
+
+  for (var i = 0; i < 6; i++) {
+    var ledge = platforms.create(i * 48, 5 * 48, 'brick');
+    ledge.body.immovable = true;
+
+    ledge.animations.add('brick', [0, 1, 2, 3], 10, true);
+    ledge.animations.add('question', [4, 5, 6, 7], 10, true);
+
+    ledge.animations.play('brick');
+
+    var ledge = platforms.create((14 - i) * 48, 2 * 48, 'brick');
+    ledge.body.immovable = true;
+
+    ledge.animations.add('brick', [0, 1, 2, 3], 10, true);
+    ledge.animations.add('question', [4, 5, 6, 7], 10, true);
+
+    ledge.animations.play('question');
+  }
 }
 
 function update() {
