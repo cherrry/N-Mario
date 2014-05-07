6dfefc3b2d6cf1ac3fc2011439852bfdd5354b3c
Cherry NG <cherry92@gmail.com>
2014-05-07 16:28:13 +0800

    add flagpole object

--- assets/js/NMario/Collectible.js
+++ assets/js/NMario/Collectible.js
@@ -1,18 +1,23 @@
 require.config({
   paths: {
     'Mushroom': 'NMario/Collectible/Mushroom',
+    'Flagpole': 'NMario/Collectible/Flagpole'
   },
   shim: {
     'Mushroom': {
       exports: 'Mushroom'
+    },
+    'Flagpole': {
+      exports: 'Flagpole'
     }
   }
 });
 
-define('Collectible', ['Mushroom'], function (Mushroom) {
+define('Collectible', ['Mushroom', 'Flagpole'], function (Mushroom, Flagpole) {
   var Collectible = {};
 
   Collectible.Mushroom = Mushroom;
+  Collectible.Flagpole = Flagpole;
 
   return Collectible;
 });
---
+++ assets/js/NMario/Collectible/Flagpole.js
@@ -0,0 +1,29 @@
+require.config({
+  paths: {
+    'Phaser': '../libs/phaser/phaser.min'
+  },
+  shim: {
+    'Phaser': {
+      exports: 'Phaser'
+    }
+  }
+});
+
+define('Flagpole', ['Phaser'], function (Phaser) {
+  var Flagpole = function(game, objects, x, y, attr) {
+    Phaser.Sprite.call(this, game, x * 16 * localStorage.scale, y * 16 * localStorage.scale, 'flagpole', 0);
+    objects.add(this);
+
+    this.scale.setTo(localStorage.scale, localStorage.scale);
+    this.body.immovable = true;
+
+    this.body.setSize(4, 111, 22 * localStorage.scale, 17 * localStorage.scale);
+
+    this.broadcast = function (socket) {};
+  };
+
+  Flagpole.prototype = Object.create(Phaser.Sprite.prototype);
+  Flagpole.prototype.constructor = Flagpole;
+
+  return Flagpole;
+});
