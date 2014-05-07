74decc5fb6ff2dac92f982e8e190fbb7d89b1550
Cherry NG <cherry92@gmail.com>
2014-05-07 14:44:02 +0800

    fix body.velocity of mushroom

--- assets/js/NMario/Collectible/Mushroom.js
+++ assets/js/NMario/Collectible/Mushroom.js
@@ -47,8 +47,8 @@ define('Mushroom', ['BaseCollectible'], function (BaseCollectible) {
         self.body.x = lastestPhysics.position.x * localStorage.scale;
         self.body.y = lastestPhysics.position.y * localStorage.scale;
 
-        self.velocity.x = lastestPhysics.position.x * localStorage.scale;
-        self.velocity.y = lastestPhysics.position.y * localStorage.scale;
+        self.body.velocity.x = lastestPhysics.position.x * localStorage.scale;
+        self.body.velocity.y = lastestPhysics.position.y * localStorage.scale;
 
         lastestPhysics = null;
       }
