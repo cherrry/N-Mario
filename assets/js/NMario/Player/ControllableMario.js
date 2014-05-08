58e4868d62a737539710f1ba98cc6180ad0b51dd
Cherry NG <cherry92@gmail.com>
2014-05-08 15:56:15 +0800

    remove target

--- assets/js/NMario/Player/ControllableMario.js
+++ assets/js/NMario/Player/ControllableMario.js
@@ -26,7 +26,7 @@ define('ControllableMario', ['Phaser', 'Mario'], function (Phaser, Mario) {
           case 'Mushroom':
             //If it is hit by player from above, kill the mushroom
             if (target.body.touching.up == true) {
-              target.collected();
+              // target.collected();
               self.body.velocity.y = -0.8 * self.body.velocity.y;
               self.send('player collect object', { id: target.id });
             } else { //Otherwise, hit the player
@@ -34,11 +34,11 @@ define('ControllableMario', ['Phaser', 'Mario'], function (Phaser, Mario) {
             }
             break;
           case 'Coin':
-            target.collected();
+            // target.collected();
             self.send('player collect object', { id: target.id });
             break;
           case 'Power-Up':
-            target.collected();
+            // target.collected();
             self.send('player collect object', { id: target.id });
             break;
         }
