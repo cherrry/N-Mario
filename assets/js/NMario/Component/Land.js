2f937b202a39339db781659822569c9931f63b70
Cherry NG <cherry92@gmail.com>
2014-05-14 01:41:48 +0800

    change land to tilesprite

--- assets/js/NMario/Component/Land.js
+++ assets/js/NMario/Component/Land.js
@@ -11,16 +11,22 @@ require.config({
 
 define('Land', ['Phaser'], function (Phaser) {
   var Land = function(game, objects, x, y, attr) {
-    Phaser.Sprite.call(this, game, x * 16 * localStorage.scale, y * 16 * localStorage.scale, 'land', 1);
+    console.log(attr.repeat);
+
+    attr.repeat = attr.repeat || { x: 1, y: 1 };
+    attr.frame = attr.frame || 1;
+
+    Phaser.TileSprite.call(this, game, x * 16 * localStorage.scale, y * 16 * localStorage.scale, attr.repeat.x * 16 * localStorage.scale, attr.repeat.y * 16 * localStorage.scale, 'land', attr.frame);
+
     objects.add(this);
 
     this.scale.setTo(localStorage.scale, localStorage.scale);
     this.body.immovable = true;
 
     this.anchor.setTo(0, 0);
-    this.body.setSize(16, 16, 0, 0);
+    this.body.setSize(attr.repeat.x * 16, attr.repeat.y * 16, 0, 0);
   };
-  Land.prototype = Object.create(Phaser.Sprite.prototype);
+  Land.prototype = Object.create(Phaser.TileSprite.prototype);
   Land.prototype.constructor = Land;
   Land.prototype.Type = 'Land';
 
