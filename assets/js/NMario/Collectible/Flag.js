be8a900d4f48cd014f230b4b8784a23839016bf8
Fishball Ng <fishballium@gmail.com>
2014-05-13 18:27:13 +0800

    Add flag component.

---
+++ assets/js/NMario/Collectible/Flag.js
@@ -0,0 +1,51 @@
+require.config({
+  paths: {
+    'BaseCollectible': 'NMario/Collectible/BaseCollectible'
+  },
+  shim: {
+    'Phaser': {
+      exports: 'Phaser'
+    }
+  }
+});
+
+define('Flag', ['BaseCollectible'], function (BaseCollectible) {
+  var Flag = function(game, objects, x, y, attr) {
+    var self = this;
+    var can_collect = true;
+
+    BaseCollectible.call(this, game, objects, x, y, attr, 'flag');
+    objects.add(this);
+
+    this.scale.setTo(localStorage.scale, localStorage.scale);
+    this.body.immovable = true;
+
+    this.body.allowGravity = false;
+
+    this.body.setSize(4, 111, 22 * localStorage.scale, 17 * localStorage.scale);
+
+    this.broadcast = function (socket) {};
+
+    this.collected = function (player, collect_index) {
+      if (player.Type == 'ControllableMario' && collect_index == 0 && can_collect) {
+        //Ask player to act 'flag'
+        player.send('player flag', {x: this.body.x});
+
+        // Set flag falling
+        this.body.setSize(4, 15, 22 * localStorage.scale, 20 * localStorage.scale);
+        this.body.immovable = false;
+        this.body.maxVelocity.y = 100 * localStorage.scale;
+        this.body.velocity.y = 100 * localStorage.scale;
+        
+        //player.send('end game', {});
+        can_collect = false;
+      }
+    };
+  };
+
+  Flag.prototype = Object.create(BaseCollectible.prototype);
+  Flag.prototype.constructor = Flag;
+  Flag.prototype.Type = 'Flag';
+
+  return Flag;
+});
