836eeea6485488feaf6b244807675caada222664
Fishball Ng <fishballium@gmail.com>
2014-05-14 18:31:16 +0800

    Send Type of collected bojects

--- assets/js/NMario/Player/ControllableMario.js
+++ assets/js/NMario/Player/ControllableMario.js
@@ -28,37 +28,37 @@ define('ControllableMario', ['Phaser', 'Mario'], function (Phaser, Mario) {
             if (target.body.touching.up == true) {
               // target.collected();
               self.body.velocity.y = -0.8 * self.body.velocity.y;
-              self.send('player collect object', { id: target.id });
+              self.send('player collect object', { id: target.id, Type: target.Type });
             } else { //Otherwise, hit the player
               this.hit();
             }
             break;
           case 'Coin':
             // target.collected();
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'Power-Up':
             // target.collected();
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'Brick':
             if (target.body.touching.down == true) {
-              self.send('player collect object', { id: target.id });
+              self.send('player collect object', { id: target.id, Type: target.Type });
             }
             break;
           case 'Flag':
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'Boat':
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'LifeUp':
             // target.collected();
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'LifeDown':
             // target.collected();
-            self.send('player collect object', { id: target.id });
+            self.send('player collect object', { id: target.id, Type: target.Type });
             break;
           case 'Water':
             self.send('player die', { id: target.id });
